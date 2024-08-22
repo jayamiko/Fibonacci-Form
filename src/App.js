e2245752a3e4675ba011c01ab276859db615f328
Jaya Miko <user@users-MacBook-Pro.local>
2024-08-22 14:46:23 +0700

    FEAT: Create Fibonacci Form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FibonacciTable from "./components/FibonacciTable";
+
+function App() {
+  return (
+    <div className="App">
+      <FibonacciTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/fibonacci.js
@@ -0,0 +1,9 @@
+const fibonacci = (n) => {
+  const sequence = [0, 1];
+  for (let i = 2; i < n; i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
+  }
+  return sequence;
+};
+
+export default fibonacci;
