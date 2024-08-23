119ab6b5f8ae9fa755096122601f916f3ec93231
Jaya Miko <user@users-MacBook-Pro.local>
2024-08-23 08:34:19 +0700

    Set Port and visible 0

--- src/components/FibonacciTable.jsx
+++ src/components/FibonacciTable.jsx
@@ -13,14 +13,17 @@ const FibonacciTable = ({ data }) => {
       <tbody>
         {data.map((row, rowIndex) => (
           <tr key={rowIndex}>
-            {row.map((cell, colIndex) => (
-              <td
-                key={colIndex}
-                style={{ padding: "10px", textAlign: "center" }}
-              >
-                {cell}
-              </td>
-            ))}
+            {row.map((cell, colIndex) => {
+              console.log(cell);
+              return (
+                <td
+                  key={colIndex}
+                  style={{ padding: "10px", textAlign: "center" }}
+                >
+                  {cell === "" ? "0" : cell}
+                </td>
+              );
+            })}
           </tr>
         ))}
       </tbody>
